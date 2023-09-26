65de6fa50e6823a4d4a6a07950761486c96b7a86
WAIXv <93478503+WAIXv@users.noreply.github.com>
2023-09-26 16:11:47 +0800

    Project Init

---
+++ Source/Ludum_2023/Ludum_2023.cpp
@@ -0,0 +1,7 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Ludum_2023.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Ludum_2023, "Ludum_2023" );
+ 

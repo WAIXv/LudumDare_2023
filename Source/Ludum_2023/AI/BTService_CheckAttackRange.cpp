7c5d6cdc55135488d46219aa6990f9368d081781
WAIXv <93478503+WAIXv@users.noreply.github.com>
2023-09-28 16:04:55 +0800

    AI Chase Player

---
+++ Source/Ludum_2023/AI/BTService_CheckAttackRange.cpp
@@ -0,0 +1,37 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "BTService_CheckAttackRange.h"
+
+#include "AIController.h"
+#include "BehaviorTree/BlackboardComponent.h"
+
+void UBTService_CheckAttackRange::TickNode(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, float DeltaSeconds)
+{
+	Super::TickNode(OwnerComp, NodeMemory, DeltaSeconds);
+
+	UBlackboardComponent* BlackboardComponent = OwnerComp.GetBlackboardComponent();
+	if (ensure(BlackboardComponent))
+	{
+		//获取TargetActor
+		AActor* TargetActor = Cast<AActor>(BlackboardComponent->GetValueAsObject(CheckTarget.SelectedKeyName));
+		if (TargetActor)
+		{
+			//获取自身Pawn位置
+			AAIController* MyController = OwnerComp.GetAIOwner();
+			if (MyController)
+			{
+				APawn* MyPawn = MyController->GetPawn();
+				if (MyPawn)
+				{
+					//计算距离
+					float Distance = FVector::Distance(MyPawn->GetActorLocation(),TargetActor->GetActorLocation());
+
+					bool bInAttackRange = Distance < AttackRangeRadium;
+
+					BlackboardComponent->SetValueAsBool(AttackRangeKey.SelectedKeyName,bInAttackRange);
+				}
+			}
+		}
+	}
+}
---
+++ Source/PluginExtension/ALS_Extension/Private/ALS_Extension.cpp
@@ -0,0 +1,17 @@
+﻿#include "ALS_Extension.h"
+
+#define LOCTEXT_NAMESPACE "FALS_ExtensionModule"
+
+void FALS_ExtensionModule::StartupModule()
+{
+    
+}
+
+void FALS_ExtensionModule::ShutdownModule()
+{
+    
+}
+
+#undef LOCTEXT_NAMESPACE
+    
+IMPLEMENT_MODULE(FALS_ExtensionModule, ALS_Extension)
